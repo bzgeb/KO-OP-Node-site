57886699f17623ab6f04d4eb573c0c94dd07b0fd
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-02-14 13:49:49 -0500

    Subscription and re-subscription will now work.

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var express = require('express')
 var mc_api_key = process.env.MC_API_KEY;
 var mc_newsletter_id = process.env.MC_NEWSLETTER_ID;
 
+var mc_interest_group = "Skipping Stones Release Announcement";
+
 try { 
     var api = new MailChimpAPI(mc_api_key, { version : '1.3', secure : false });
 } catch (error) {
@@ -55,20 +57,51 @@ app.get('/', function (req, res) {
 app.post('/mailing_list/subscribe', function (req, res) {
   var email = req.body.email;
 
+  api.listMemberInfo({ id: mc_newsletter_id, email_address: [email]},
+      function(error, data) {
+          console.log("Error: " + error);
+          console.log("Data: " + JSON.stringify(data));
+      });
+
   api.listSubscribe({ id: mc_newsletter_id, 
                       email_address: email,
                       merge_vars: {EMAIL: email,
                                   FNAME: '',
                                   LNAME: '',
-                                  GROUPINGS: [{id: 8953, groups: ""}]}},
+                                  GROUPINGS: [{id: 8953, groups: mc_interest_group}]}},
     function(error, data) {
       if (error) {
-        console.log(error.message);
-        res.send("<p>Subscription failed.  Please make sure your email address is valid.</p>");
+        var error_message = "";
+        console.log(error.code);
+
+        // Invalid Email Error
+        if ( error.code == 502 ) {
+            error_message = "Subscription failed.  Please verify that your email address is correct.";
+        }
+        // Already a Member, so we update his interests
+        else if ( error.code == 214 ) {
+           error_message = "You've been added to the " + mc_interest_group + " group.";
+           api.listUpdateMember({ id: mc_newsletter_id, 
+                                  email_address: email,
+                                  merge_vars: {EMAIL: email,
+                                               GROUPINGS: [{id: 8953, groups: mc_interest_group}]},
+                                  replace_interests: false},
+                                  function(error, data) {
+                                      if (error) {
+                                          console.log(error.message);
+                                      }
+                                      else {
+                                          console.log(JSON.stringify(data));
+                                      }
+                                  });
+
+        }
+
+        res.send("<p>" + error_message + "</p>");
       }
       else {
         console.log(JSON.stringify(data)); // Do something with your data! 
-        res.send("<p>You are subscribed.  Please check your email for a confirmation email.</p>");
+        res.send("<p>You are subscribed.  Please check your email for a confirmation.</p>");
       }
     });
 });
