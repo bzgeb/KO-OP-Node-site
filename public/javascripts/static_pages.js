382828f0bc1f52c27e000067ffe4b37209fe11b3
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-06-13 17:50:52 -0400

    Fixed phone mailing list subscription

--- public/javascripts/static_pages.js
+++ public/javascripts/static_pages.js
@@ -6,10 +6,22 @@
     return $("#response").html("<p>Sending...</p>");
   });
 
+  $("#mail-form-phone").bind("ajax:complete", function(xhr, status) {
+    return $("#response-phone").html(status.responseText);
+  }).bind("ajax:beforeSend", function() {
+    return $("#response-phone").html("<p>Sending...</p>");
+  });
+
   $("#general-mail-form").bind("ajax:complete", function(xhr, status) {
     return $("#general-response").html(status.responseText);
   }).bind("ajax:beforeSend", function() {
     return $("#general-response").html("<p>Sending...</p>");
+  });
+
+  $("#general-mail-form-phone").bind("ajax:complete", function(xhr, status) {
+    return $("#general-response-phone").html(status.responseText);
+  }).bind("ajax:beforeSend", function() {
+    return $("#general-response-phone").html("<p>Sending...</p>");
   });
 
 }).call(this);
